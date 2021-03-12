0bf9248c0e3141cd2630d73e2f0c1a6d19b61dda
vivek <vivek@athenaeum.in>
2021-03-12 22:13:11 +0530

    done

--- src/app/demo.service.spec.ts
+++ src/app/demo.service.spec.ts
@@ -1,28 +1,21 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { DemoService } from './demo.service';
 
 describe('DemoService', () => {
   let service: DemoService;
-  let httpClient: HttpClient;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-    });
-    httpClient = TestBed.inject(HttpClient);
-  });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('#getTodos should return actual array from api', (done: DoneFn) => {
-    service = new DemoService(httpClient);
+  it('#getTodos should return result from mock', (done: DoneFn) => {
+    const mockHttpClient = { get: (url: string) => of([]) };
+    service = new DemoService(mockHttpClient as HttpClient);
     service.getTodos().subscribe((v) => {
       expect(v).toBeInstanceOf(Array);
-      expect(v.length).toBe(200);
       done();
     });
   });
